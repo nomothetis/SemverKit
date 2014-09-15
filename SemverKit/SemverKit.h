627af5e319608fee93e687979d4c25ab743caabe
Alexandros Salazar <nomothetis@gmail.com>
2014-09-15 14:10:32 -0400

    Initial Commit

---
+++ SemverKit/SemverKit.h
@@ -0,0 +1,19 @@
+//
+//  SemverKit.h
+//  SemverKit
+//
+//  Created by Salazar, Alexandros on 9/15/14.
+//  Copyright (c) 2014 nomothetis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SemverKit.
+FOUNDATION_EXPORT double SemverKitVersionNumber;
+
+//! Project version string for SemverKit.
+FOUNDATION_EXPORT const unsigned char SemverKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SemverKit/PublicHeader.h>
+
+
