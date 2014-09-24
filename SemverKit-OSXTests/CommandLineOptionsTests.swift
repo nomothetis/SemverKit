a2d6e082391b5135432c1c74e3884a3ddef08301
Alexandros Salazar <nomothetis@gmail.com>
2014-09-24 16:23:19 -0400

    Fixes a filename and adds a test file.

---
+++ SemverKit-OSXTests/CommandLineOptionsTests.swift
@@ -0,0 +1,9 @@
+//
+//  CommandLineOptionsTests.swift
+//  SemverKit
+//
+//  Created by Salazar, Alexandros on 9/24/14.
+//  Copyright (c) 2014 nomothetis. All rights reserved.
+//
+
+import Foundation
